ad2251fffa5050d03db087b76a489278eeaac6ca
Jan Etschel <jan.etschel@icloud.com>
2024-06-14 09:32:27 +0200

    Initial Commit

---
+++ food/foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodApp.swift
+//  food
+//
+//  Created by Jan Etschel on 14.06.24.
+//
+
+import SwiftUI
+
+@main
+struct foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
